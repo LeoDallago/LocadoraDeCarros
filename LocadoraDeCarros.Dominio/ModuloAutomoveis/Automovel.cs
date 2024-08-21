4d264d618b4ca647106b165d4648396c43ed3243
LeoDallago <leodallago@outlook.com>
2024-08-21 16:48:12 -0300

    implementado dominio e testes unitarios

---
+++ LocadoraDeCarros.Dominio/ModuloAutomoveis/Automovel.cs
@@ -0,0 +1,37 @@
+﻿using LocadoraDeCarros.Dominio.Compartilhado;
+using LocadoraDeCarros.Dominio.ModuloGrupoAutomoveis;
+
+namespace LocadoraDeCarros.Dominio.ModuloAutomoveis;
+
+public class Automovel : EntidadeBase
+{
+    public string Marca { get; set; }
+    
+    public string Modelo { get; set; }
+    
+    public string Cor { get; set; }
+    
+    public string TipoCombustivel { get; set; }
+    
+    public string CapacidadeCombustivel { get; set; }
+    
+    public string Ano { get; set; }
+    
+    public GrupoAutomoveis Grupo { get; set; }
+
+    public Automovel()
+    {
+        
+    }
+
+    public Automovel(string marca, string modelo, string cor, string tipoCombustivel, string capacidadeCombustivel, string ano, GrupoAutomoveis grupo)
+    {
+        Marca = marca;
+        Modelo = modelo;
+        Cor = cor;
+        TipoCombustivel = tipoCombustivel;
+        CapacidadeCombustivel = capacidadeCombustivel;
+        Ano = ano;
+        Grupo = grupo;
+    }
+}
---
+++ LocadoraDeCarros.Testes/Unitarios/ModuloAutomovel/TesteUnitario.cs
@@ -0,0 +1,54 @@
+﻿using LocadoraDeCarros.Dominio.ModuloAutomoveis;
+using LocadoraDeCarros.Dominio.ModuloGrupoAutomoveis;
+
+namespace LocadoraDeCarros.Testes.Unitarios.ModuloAutomovel;
+
+[TestClass]
+[TestCategory("Teste Unitario Automovel")]
+public class TesteUnitario
+{
+    [TestMethod]
+    public void Deve_Inserir_Veiculo()
+    {
+        //Arrange
+        GrupoAutomoveis grupoAutomoveis = new GrupoAutomoveis("grupoteste");
+        Automovel novoAutomovel;
+        
+        //Act
+        novoAutomovel = new Automovel(
+            "marca",
+            "modelo",
+            "cor",
+            "combustivel",
+            "capacidade",
+            "2000",
+            grupoAutomoveis
+            );
+        
+        //Assert
+        Assert.IsNotNull(novoAutomovel);
+        Assert.AreEqual(novoAutomovel.Marca, "marca");
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Veiculo()
+    {
+        //Arrange
+        GrupoAutomoveis grupoAutomoveis = new GrupoAutomoveis("grupoteste");
+        Automovel novoAutomovel = new Automovel(
+            "marca",
+            "modelo",
+            "cor",
+            "combustivel",
+            "capacidade",
+            "2000",
+            grupoAutomoveis
+        );
+        
+        //Act
+        novoAutomovel.Marca = "Fiat";
+        
+        //Assert
+        Assert.AreEqual(novoAutomovel.Marca, "Fiat");
+    }
+}
