ef04b16525af198a7672ccb6e13a9bbf6a0d76c8
LeoDallago <leodallago@outlook.com>
2024-08-24 14:59:20 -0300

    implementado ViewModels e Mapping (taxas e serviços)

---
+++ LocadoraDeCarros.WebApp/Mapping/Taxa.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LocadoraDeCarros.WebApp.Models;
+
+namespace LocadoraDeCarros.WebApp.Mapping;
+
+public class Taxa : Profile
+{
+    public Taxa()
+    {
+        CreateMap<InserirTaxaViewModel, Dominio.ModuloTaxa.Taxa>();
+        CreateMap<EditarTaxaViewModel, Dominio.ModuloTaxa.Taxa>();
+
+        CreateMap<Dominio.ModuloTaxa.Taxa, EditarTaxaViewModel>();
+        CreateMap<Dominio.ModuloTaxa.Taxa, ListarTaxaViewModel>();
+    }
+}
---
+++ LocadoraDeCarros.WebApp/Models/TaxaViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocadoraDeCarros.WebApp.Models;
+
+public class InserirTaxaViewModel
+{
+    [Required(ErrorMessage = "O nome da taxa ou serviço é obrigatório")]
+    public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "O preço é obrigatório")]
+    public decimal Preco{ get; set; }
+    
+    [Required(ErrorMessage = "O tipo de plano é obrigatório")]
+    public string PlanoCobranca{ get; set; }
+}
+
+public class EditarTaxaViewModel
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O nome da taxa ou serviço é obrigatório")]
+    public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "O preço é obrigatório")]
+    public decimal Preco{ get; set; }
+    
+    [Required(ErrorMessage = "O tipo de plano é obrigatório")]
+    public string PlanoCobranca{ get; set; }
+}
+
+public class ListarTaxaViewModel
+{
+    public int Id { get; set; }
+    
+    public string Nome { get; set; }
+    
+    public decimal Preco{ get; set; }
+    
+    public string PlanoCobranca{ get; set; }
+}
