ea9f2e9c707508f700b9ecb97d2007b78668893e
LeoDallago <leodallago@outlook.com>
2024-08-26 14:27:54 -0300

    implementado entidade e testes unitarios (clientes)

---
+++ LocadoraDeCarros.Dominio/ModuloCliente/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraDeCarros.Dominio.Compartilhado;
+
+namespace LocadoraDeCarros.Dominio.ModuloCliente;
+
+public class Cliente : EntidadeBase
+{
+    public string Nome { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Telefone { get; set; }
+    
+    public string Estado { get; set; }
+    
+    public string Cidade { get; set; }
+    
+    public string Bairro { get; set; }
+    
+    public string Rua { get; set; }
+    
+    public string Numero { get; set; }
+    
+    public string TipoCliente { get; set; }
+
+    public Cliente()
+    {
+        
+    }
+    
+    public Cliente(string nome, string email, string telefone, string estado, string cidade, string bairro, string rua, string numero, string tipoCliente)
+    {
+        Nome = nome;
+        Email = email;
+        Telefone = telefone;
+        Estado = estado;
+        Cidade = cidade;
+        Bairro = bairro;
+        Rua = rua;
+        Numero = numero;
+        TipoCliente = tipoCliente;
+    }
+    
+}
---
+++ LocadoraDeCarros.Testes/Unitarios/ModuloCliente/TesteUnitario.cs
@@ -0,0 +1,56 @@
+﻿using LocadoraDeCarros.Dominio.ModuloCliente;
+
+namespace LocadoraDeCarros.Testes.Unitarios.ModuloCliente;
+
+[TestClass]
+[TestCategory("Testes Unitarios Modulo Cliente")]
+public class TesteUnitario
+{
+    [TestMethod]
+    public void Deve_Inserir_Cliente()
+    {
+        //Arrange
+        Cliente novoCLiente;
+        
+        //Act
+        novoCLiente = new Cliente
+        {
+            Nome = "Juan",
+            Email = "teste@test.com",
+            Telefone = "123456789",
+            Estado = "Sc",
+            Cidade = "Lages",
+            Bairro = "Centro",
+            Rua = "teste",
+            Numero = "123",
+            TipoCliente = "Fisica"
+        };
+        
+        //Assert
+        Assert.AreEqual(novoCLiente.Nome, "Juan");
+    }
+
+    [TestMethod]
+    public void Deve_Editar_Cliente()
+    {
+        //Arrange
+        Cliente novoCLientene = new Cliente
+        {
+            Nome = "Juan",
+            Email = "teste@test.com",
+            Telefone = "123456789",
+            Estado = "Sc",
+            Cidade = "Lages",
+            Bairro = "Centro",
+            Rua = "teste",
+            Numero = "123",
+            TipoCliente = "Fisica"
+        };
+        
+        //Act
+        novoCLientene.Cidade = "Chapecó";
+        
+        //Assert
+        Assert.AreNotEqual(novoCLientene.Cidade,"Lages");
+    }
+}
