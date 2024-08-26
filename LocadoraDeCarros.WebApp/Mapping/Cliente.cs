0edd3a4eb93d6a3ab9d48b49a1cb03ffc79cd1ce
LeoDallago <leodallago@outlook.com>
2024-08-26 15:35:50 -0300

    implementado mapping e ViewModel (Cliente)

---
+++ LocadoraDeCarros.WebApp/Mapping/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using LocadoraDeCarros.WebApp.Models;
+
+namespace LocadoraDeCarros.WebApp.Mapping;
+
+public class Cliente : Profile
+{
+    public Cliente()
+    {
+        CreateMap<InserirClienteViewModel, Dominio.ModuloCliente.Cliente>();
+        CreateMap<EditarClienteViewModel, Dominio.ModuloCliente.Cliente>();
+
+        CreateMap<Dominio.ModuloCliente.Cliente, EditarClienteViewModel>();
+        CreateMap<Dominio.ModuloCliente.Cliente, ListarAutomovelViewModel>();
+        CreateMap<Dominio.ModuloCliente.Cliente, DetalhesAutomovelViewModel>();
+    }
+}
---
+++ LocadoraDeCarros.WebApp/Models/ClienteViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LocadoraDeCarros.WebApp.Models;
+
+public class InserirClienteViewModel
+{
+    [Required(ErrorMessage = "O Nome é obrigatório")]
+    public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "O Email é obrigatório")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "O Telefone é obrigatório")]
+    public string Telefone { get; set; }
+    
+    [Required(ErrorMessage = "O Estado é obrigatório")]
+    public string Estado { get; set; }
+    
+    [Required(ErrorMessage = "A Cidade é obrigatória")]
+    public string Cidade { get; set; }
+    
+    [Required(ErrorMessage = "O Bairro é obrigatório")]
+    public string Bairro { get; set; }
+    
+    [Required(ErrorMessage = "A Rua é obrigatória")]
+    public string Rua { get; set; }
+    
+    [Required(ErrorMessage = "O Numero da Casa é obrigatório")]
+    public string Numero { get; set; }
+    
+    [Required(ErrorMessage = "O Tipo de Cliente é obrigatório")]
+    public string TipoCliente { get; set; }
+}
+
+public class EditarClienteViewModel
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O Nome é obrigatório")]
+    public string Nome { get; set; }
+    
+    [Required(ErrorMessage = "O Email é obrigatório")]
+    public string Email { get; set; }
+    
+    [Required(ErrorMessage = "O Telefone é obrigatório")]
+    public string Telefone { get; set; }
+    
+    [Required(ErrorMessage = "O Estado é obrigatório")]
+    public string Estado { get; set; }
+    
+    [Required(ErrorMessage = "A Cidade é obrigatória")]
+    public string Cidade { get; set; }
+    
+    [Required(ErrorMessage = "O Bairro é obrigatório")]
+    public string Bairro { get; set; }
+    
+    [Required(ErrorMessage = "A Rua é obrigatória")]
+    public string Rua { get; set; }
+    
+    [Required(ErrorMessage = "O Numero da Casa é obrigatório")]
+    public string Numero { get; set; }
+    
+    [Required(ErrorMessage = "O Tipo de Cliente é obrigatório")]
+    public string TipoCliente { get; set; }
+}
+
+public class ListarClienteViewModel
+{
+    public int Id { get; set; }
+    
+    public string Nome { get; set; }
+    
+    public string Telefone { get; set; }
+    
+    public string TipoCliente { get; set; }
+}
+
+public class DetalhesClienteViewModel
+{
+    public int Id { get; set; }
+    
+    public string Nome { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Telefone { get; set; }
+    
+    public string Estado { get; set; }
+    
+    public string Cidade { get; set; }
+    
+    public string Bairro { get; set; }
+    
+    public string Rua { get; set; }
+    
+    public string Numero { get; set; }
+    
+    public string TipoCliente { get; set; }
+}
