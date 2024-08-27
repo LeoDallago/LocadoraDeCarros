c4e576fd47f58512d31941221d68ae0351fa8b1e
LeoDallago <leodallago@outlook.com>
2024-08-27 16:42:39 -0300

    implementado camada aplicação (cliente)

---
+++ LocadoraDeCarros.Aplicacao/Servicos/CondutorService.cs
@@ -0,0 +1,96 @@
+﻿using FluentResults;
+using LocadoraDeCarros.Dominio.ModuloCliente;
+using LocadoraDeCarros.Dominio.ModuloCondutor;
+
+namespace LocadoraDeCarros.Aplicacao.Servicos;
+
+public class CondutorService
+{
+    private readonly IRepositorioCondutor repositorioCondutor;
+    private readonly IRepositorioCliente repositorioCliente;
+
+    public CondutorService(IRepositorioCondutor repositorioCondutor, IRepositorioCliente repositorioCliente)
+    {
+        this.repositorioCondutor = repositorioCondutor;
+        this.repositorioCliente = repositorioCliente;
+    }
+
+
+    public Result<Condutor> Inserir(Condutor condutor, int clienteId)
+    {
+        var clienteSelecionado = repositorioCliente.SelecionarPorId(clienteId);
+
+        if (clienteSelecionado is null)
+        {
+            return Result.Fail("Cliente nao encontrado!");
+        }
+        
+        condutor.Cliente = clienteSelecionado;
+        
+        repositorioCondutor.Inserir(condutor);
+        
+        return Result.Ok(condutor);
+    }
+
+    public Result<Condutor> Editar(int condutorId, Condutor condutor, int clienteId)
+    {
+        var clienteSelecionado = repositorioCliente.SelecionarPorId(clienteId);
+
+        if (clienteSelecionado is null)
+        {
+            return Result.Fail("Cliente nao encontrado!");
+        }
+        
+        var condutorSelecionado = repositorioCondutor.SelecionarPorId(condutorId);
+
+        if (condutorSelecionado is null)
+        {
+            return Result.Fail("Condutor nao encontrado!");
+        }
+        
+        condutorSelecionado.Nome = condutor.Nome;
+        condutorSelecionado.Email = condutor.Email;
+        condutorSelecionado.Telefone = condutor.Telefone;
+        condutorSelecionado.CPF = condutor.CPF;
+        condutorSelecionado.CNH = condutor.CNH;
+        condutorSelecionado.Validade = condutor.Validade;
+        condutorSelecionado.Cliente = clienteSelecionado;
+        
+        repositorioCondutor.Editar(condutorSelecionado);
+        
+        return Result.Ok(condutorSelecionado);
+    }
+
+    public Result<Condutor> Excluir(int id)
+    {
+        var condutor = repositorioCondutor.SelecionarPorId(id);
+
+        if (condutor is null)
+        {
+            return Result.Fail("Condutor nao encontrado!");
+        }
+        
+        repositorioCondutor.Excluir(condutor);
+        
+        return Result.Ok();
+    }
+
+    public Result<Condutor> SelecionarPorId(int id)
+    {
+        var condutor = repositorioCondutor.SelecionarPorId(id);
+        
+        if (condutor is null)
+        {
+            return Result.Fail("Condutor nao encontrado!");
+        }
+        
+        return Result.Ok(condutor);
+    }
+
+    public Result<List<Condutor>> SelecionarTodos()
+    {
+        var condutores = repositorioCondutor.SelecionarTodos();
+        
+        return Result.Ok(condutores);
+    }
+}
--- LocadoraDeCarros.WebApp/Program.cs
+++ LocadoraDeCarros.WebApp/Program.cs
@@ -3,12 +3,14 @@ using LocadoraDeCarros.Aplicacao;
 using LocadoraDeCarros.Aplicacao.Servicos;
 using LocadoraDeCarros.Dominio.ModuloAutomoveis;
 using LocadoraDeCarros.Dominio.ModuloCliente;
+using LocadoraDeCarros.Dominio.ModuloCondutor;
 using LocadoraDeCarros.Dominio.ModuloGrupoAutomoveis;
 using LocadoraDeCarros.Dominio.ModuloPlanos;
 using LocadoraDeCarros.Dominio.ModuloTaxa;
 using LocadoraDeCarros.Infra.Compartilhado;
 using LocadoraDeCarros.Infra.ModuloAutomovel;
 using LocadoraDeCarros.Infra.ModuloCliente;
+using LocadoraDeCarros.Infra.ModuloCondutor;
 using LocadoraDeCarros.Infra.ModuloGrupoAutomoveis;
 using LocadoraDeCarros.Infra.ModuloPlanos;
 using LocadoraDeCarros.Infra.ModuloTaxa;
@@ -28,13 +30,14 @@ builder.Services.AddScoped<IRepositorioAutomovel, RepositorioAutomovel>();
 builder.Services.AddScoped<IRepositorioPlanos, RepositorioPlanos>();
 builder.Services.AddScoped<IRepositorioTaxa, RepositorioTaxa>();
 builder.Services.AddScoped<IRepositorioCliente, RepositorioCliente>();
+builder.Services.AddScoped<IRepositorioCondutor, RepositorioCondutor>();
 
 builder.Services.AddScoped<GrupoAutomoveisService>();
 builder.Services.AddScoped<AutomoveisService>();
 builder.Services.AddScoped<PlanosService>();
 builder.Services.AddScoped<TaxaService>();
 builder.Services.AddScoped<ClienteService>();
-
+builder.Services.AddScoped<CondutorService>();
 
 builder.Services.AddAutoMapper(cfg =>
 {
